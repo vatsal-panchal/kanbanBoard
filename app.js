5970cc292fe49234c93d541df902eaeeab051308
vatsal-panchal <panchalvatsal2025@gmail.com>
2026-01-04 17:00:17 +0530

    progress

--- app.js
+++ app.js
@@ -1,41 +1,76 @@
-// const todo = document.querySelector("#todo");
-// const progress = document.querySelector("#progress");
-// const done = document.querySelector("#done");
-// const tasks = document.querySelectorAll(".task");
-// let dragElement = null;
-// tasks.forEach((task) => {
-//   task.addEventListener("drag", (e) => {
-//     dragElement = task;
-//   });
-// });
+const todo = document.querySelector("#todo");
+const progress = document.querySelector("#progress");
+const done = document.querySelector("#done");
+const tasks = document.querySelectorAll(".task");
+let dragElement = null;
+tasks.forEach((task) => {
+  task.addEventListener("drag", (e) => {
+    dragElement = task;
+  });
+});
 
-// function addDragEventsOnColumn(column) {
-//   column.addEventListener("dragenter", (e) => {
-//     e.preventDefault();
-//     column.classList.add("hover-over");
-//   });
+function addDragEventsOnColumn(column) {
+  column.addEventListener("dragenter", (e) => {
+    e.preventDefault();
+    column.classList.add("hover-over");
+  });
 
-//   column.addEventListener("dragleave", (e) => {
-//     e.preventDefault();
-//     column.classList.remove("hover-over");
-//   });
+  column.addEventListener("dragleave", (e) => {
+    e.preventDefault();
+    column.classList.remove("hover-over");
+  });
 
-//   // iss code se browser allow karta he ki aap task add kar sakte ho
-//   column.addEventListener("dragover", (e) => {
-//     e.preventDefault();
-//   });
+  column.addEventListener("dragover", (e) => {
+    e.preventDefault();
+  });
 
-//   column.addEventListener("drop", (ok) => {
-//     ok.preventDefault();
+  column.addEventListener("drop", (e) => {
+    e.preventDefault();
+    column.appendChild(dragElement);
+    column.classList.remove("hover-over");
+  });
+}
 
-//     column.appendChild(dragElement);
-//     column.classList.remove("hover-over");
-//   });
-// }
+addDragEventsOnColumn(todo);
+addDragEventsOnColumn(progress);
+addDragEventsOnColumn(done);
 
-// addDragEventsOnColumn(todo);
-// addDragEventsOnColumn(progress);
-// addDragEventsOnColumn(done);
+// open modal logic
+const togglemodalBtn = document.querySelector("#toggle-modal");
+const modalBg = document.querySelector(".modal .bg");
+const modal = document.querySelector(".modal");
+const addTaskbBtn = document.querySelector("#add-new-task");
 
+togglemodalBtn.addEventListener("click", () => {
+  modal.classList.toggle("active");
+});
 
+modalBg.addEventListener("click", () => {
+  modal.classList.remove("active");
+});
 
+addTaskbBtn.addEventListener("click", () => {
+  const taskTitle = document.querySelector("#task-title-input").value;
+  const taskDesc = document.querySelector("#task-desc-input").value;
+  console.log(taskTitle, taskDesc);
+
+  const div = document.createElement("div");
+  div.classList.add("task");
+  div.setAttribute("draggable", true);
+
+  div.innerHTML = `
+   <h2>${taskTitle}</h2>
+            <p>${taskDesc}</p>
+              <button>delete</button>
+  `;
+
+  todo.append(div);
+
+  div.addEventListener("drag", (e) => {
+    dragElement = div;
+  });
+
+  modal.classList.remove("active");
+});
+
+// open modal logic
